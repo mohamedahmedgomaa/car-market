1e5d42de0217d6515e8d31b960f4c1e76004b506
Mohamed Gomaa <42862417+mohamedahmedgomaa@users.noreply.github.com>
2026-01-18 01:25:42 +0300

    edit bug 2

--- config/database.php
+++ config/database.php
@@ -85,14 +85,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('PG_HOST', '127.0.0.1'),
-            'port' => env('PG_PORT', '5432'),
-            'database' => env('PG_DATABASE'),
-            'username' => env('PG_USERNAME'),
-            'password' => env('PG_PASSWORD'),
-            'charset' => 'utf8',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
-            'schema' => 'public',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
 
