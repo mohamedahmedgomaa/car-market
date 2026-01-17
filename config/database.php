a8874c24d08a7eca89ae1f2bed807937183bcb9e
Mohamed Gomaa <42862417+mohamedahmedgomaa@users.noreply.github.com>
2026-01-18 00:07:50 +0300

    Change Database - edit files (change to sqlLite)

--- config/database.php
+++ config/database.php
@@ -85,16 +85,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'host' => env('PG_HOST', '127.0.0.1'),
+            'port' => env('PG_PORT', '5432'),
+            'database' => env('PG_DATABASE'),
+            'username' => env('PG_USERNAME'),
+            'password' => env('PG_PASSWORD'),
+            'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
+            'schema' => 'public',
             'sslmode' => 'prefer',
         ],
 
