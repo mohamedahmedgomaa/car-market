6bf419c712e833502f74105e73318786c58b5514
abdallah negm <113768788+Negm-8@users.noreply.github.com>
2026-05-07 17:50:39 +0300

    اا
    تت

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'message' => 'Car Market API is running 🚗',
+        'message' => 'NegmCars API is running 🚗',
         'status' => 'ok'
     ]);
 });
